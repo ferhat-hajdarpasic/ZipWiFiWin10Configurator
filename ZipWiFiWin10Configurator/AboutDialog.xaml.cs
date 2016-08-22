6fb1598a3300d36a3eead1c54d859da24eb94a44
Ferhat Hajdarpasic <ferhat.hajdarpasic@live.com.au>
2016-08-22 21:49:05 +1000

    Changed socket client.

--- ZipWiFiWin10Configurator/AboutDialog.xaml.cs
+++ ZipWiFiWin10Configurator/AboutDialog.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,6 +23,11 @@ namespace ZipWiFiWin10Configurator
         public AboutDialog()
         {
             this.InitializeComponent();
+            this.VersionTextBlock.Text = string.Format("Version: {0}.{1}.{2}.{3}",
+                    Package.Current.Id.Version.Major,
+                    Package.Current.Id.Version.Minor,
+                    Package.Current.Id.Version.Build,
+                    Package.Current.Id.Version.Revision);
         }
 
         private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
